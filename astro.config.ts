4da33bdaaab517ddc815a324fbc2ef03d8a45ce3
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-11-12 23:49:31 +0700

    feat: add math plugins

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,29 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import tailwind from '@astrojs/tailwind'
+import remarkMath from 'remark-math'
+import rehypeMathJax from 'rehype-mathjax'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://area4.github.io' : 'http://localhost:4321',
   base: '/starlight-tailwind-template',
-  experimental: {
-    contentCollectionCache: true,
+  // experimental: {
+  //  contentCollectionCache: true,
+  // },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJax],
   },
   integrations: [
     starlight({
       title: 'Starlight with Tailwind',
+      customCss: [
+        './src/styles/globals.css',
+        './src/styles/mathjax.css',
+      ],
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: 'https://github.com/AREA44/starlight-tailwind-template',
       },
       sidebar: [
         {
@@ -25,7 +35,6 @@ export default defineConfig({
           autogenerate: { directory: 'reference' },
         },
       ],
-      customCss: ['./src/styles/globals.css'],
     }),
     tailwind({ applyBaseStyles: false }),
   ],
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
-import colors from 'tailwindcss/colors'
+import type { Config } from 'tailwindcss'
 import starlightPlugin from '@astrojs/starlight-tailwind'
+import colors from 'tailwindcss/colors'
 
-/** @type {import('tailwindcss').Config} */
-export default {
+const config: Config = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
         accent: colors.indigo,
-        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
         gray: colors.zinc,
       },
     },
   },
   plugins: [starlightPlugin()],
 }
+
+export default config
