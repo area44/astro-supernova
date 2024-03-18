02804a6c017607f885b70367537047c58197b5df
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2024-03-19 01:49:40 +0700

    ignore mdx

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -6,18 +6,4 @@ export default {
   singleQuote: true,
   endOfLine: 'lf',
   plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-  overrides: [
-    {
-      files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
-      options: {
-        useTabs: false,
-      },
-    },
-    {
-      files: ['*.md', '*.mdx'],
-      options: {
-        printWidth: 80,
-      },
-    },
-  ],
 }
