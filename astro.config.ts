a2aed64378cebd00be069c9a00091ec1c9f84362
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2024-04-29 00:36:30 +0700

    Support Mermaid (Thanks @stereobooster with https://github.com/stereobooster/astro-digital-garden)

--- astro.config.ts
+++ astro.config.ts
@@ -3,19 +3,24 @@ import starlight from '@astrojs/starlight'
 import tailwind from '@astrojs/tailwind'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
+import remarkMermaidjs from 'remark-mermaidjs'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://area4.github.io' : 'http://localhost:4321',
   base: '/playastro',
   markdown: {
-    remarkPlugins: [remarkMath],
+    remarkPlugins: [remarkMath, remarkMermaidjs],
     rehypePlugins: [rehypeKatex],
   },
   integrations: [
     starlight({
       title: 'PlayAstro',
-      customCss: ['./src/styles/globals.css', 'katex/dist/katex.min.css'],
+      customCss: [
+        './src/styles/globals.css',
+        'katex/dist/katex.min.css',
+        './src/styles/mermaid.css',
+      ],
       social: {
         github: 'https://github.com/AREA44/playastro',
       },
